0d0e45d01da032be56c875335f020d06086f8b9a
Alexandru Cristian Antonescu <alexsept100@gmail.com>
2025-12-01 13:05:00 +0100

    test: not expect dublicate notice (RED)

--- src/tests/pubController.test.js
+++ src/tests/pubController.test.js
@@ -38,4 +38,29 @@ describe("Pub Logic", () => {
       })
     )
   })
+
+  it("Should NOT send dublicate notice if the event is already known", async () => {
+    const htmlEventA = `
+      <html><body><a href="/old">IT-pub: Old Event</a></body></html>
+    `
+    const htmlEventB = `
+      <html><body><a href="/new">IT-pub: New Awesome Event</a></body></html>
+    `
+
+    axios.get.mockReturnValue({ data: htmlEventA })
+    const pubWatcher = new CheckPub()
+
+    await pubWatcher.check()
+    expect(axios.post).toHaveBeenCalledTimes(1)
+
+    axios.post.mockClear()
+    await pubWatcher.check()
+
+    expect(axios.post).not.toHaveBeenCalled()
+
+    axios.get.mockReturnValue({ data: htmlEventB })
+    await pubWatcher.check()
+
+    expect(axios.post).toHaveBeenCalledTimes(1)
+  })
 })
